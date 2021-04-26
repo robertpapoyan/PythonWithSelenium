79627cabc959676fd5f4c5399fa33cde17232632
Robert Papoyan <robertpapoyan96@gmail.com>
2021-04-26 16:16:51 +0400

    Created POM of Login page/Created tests of login and homepage verification/added browser setup fixture/implemented logic of screenshoting the page in case of failed test

---
+++ PageObjects/LoginPage.py
@@ -0,0 +1,22 @@
+class LoginPage:
+    textbox_username_id="Email"
+    textbox_password_id="Password"
+    button_login_xpath="/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button"
+    link_logut_linktext="Logout"
+
+    def __init__(self,driver):
+        self.driver = driver
+
+    def setUserName(self,userName):
+        self.driver.find_element_by_id(self.textbox_username_id).clear()
+        self.driver.find_element_by_id(self.textbox_username_id).send_keys(userName)
+
+    def setPassword(self,password):
+        self.driver.find_element_by_id(self.textbox_password_id).clear()
+        self.driver.find_element_by_id(self.textbox_password_id).send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element_by_xpath(self.button_login_xpath).click()
+
+    def clickLogOut(self):
+        self.driver.find_element_by_link_tex(self.button_logout_linktext).click()
---
+++ TestCases/test_login.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from PageObjects.LoginPage import LoginPage
+
+class Test_001_Login:
+    baseURL = "https://admin-demo.nopcommerce.com/login"
+    username = "admin@yourstore.com"
+    password = "admin"
+
+    def test_homePageTitle(self,setup):
+        self.driver = setup
+        self.driver.maximize_window()
+        self.driver.get(self.baseURL)
+        actual_title = self.driver.title
+
+
+        if actual_title == "Your storesss. Login":
+            self.driver.close()
+            assert True
+        else:
+            self.driver.save_screenshot(".\\screenshots\\"+"test_homePageTitle.png")
+            self.driver.close()
+            assert False
+
+
+    def test_login(self, setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.loginPageObject = LoginPage(self.driver)
+        self.loginPageObject.setUserName(self.username)
+        self.loginPageObject.setPassword(self.password)
+        self.loginPageObject.clickLogin()
+
+        actual_title = self.driver.title
+        self.driver.close()
+
+        if actual_title == "Dashboard / nopCommerce administration":
+            assert True
+        else:
+            assert False
+
