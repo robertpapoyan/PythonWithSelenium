9ecd288acdab4e222eafbb064b87009d90d20c99
Robert Papoyan <robertpapoyan96@gmail.com>
2021-04-26 23:26:00 +0400

    Logg issue fixed

--- Utilities/customLogger.py
+++ Utilities/customLogger.py
@@ -3,9 +3,17 @@ import logging
 class LogGenerator():
     @staticmethod
     def logGenerator():
-        logging.basicConfig(filename=".\\logs\\auto.log",
-                        format='%(asctime)s: %(message)s:', datefmt='%d/%m/%Y %I:%M:%S %p')
+        # logging.basicConfig(filename=".\\logs\\auto.log",
+        #                 format='%(asctime)s: %(message)s:', datefmt='%d/%m/%Y %I:%M:%S %p')
+        # logger = logging.getLogger()
+        # logger.setLevel(logging.INFO)
+        # return logger
+
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler = logging.FileHandler(filename='.\\logs\\auto.log')
+        handler.setFormatter(formatter)
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
         return logger
 
